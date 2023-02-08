5eb8c7fbda780c3534769135c5354132e2506cc5
dkkiooko <kithekadankioko@gmail.com>
2023-02-08 13:26:16 +0300

    exported to JSON

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ gathers data from an API
-and exports to CSV
+and exports to JSON
 """
-import csv
+import json
 import requests
 from sys import argv
 
@@ -20,11 +20,16 @@ def main():
     name = r.json()['username']
     r2 = requests.get('{}todos?userId={}'.format(url, user_id)).json()
     all_tasks = [i for i in r2[:]]
-    with open(f'{user_id}.csv', 'w') as file:
-        dw = csv.writer(file, delimiter=',')
-        for element in all_tasks:
-            dw.writerow([user_id] + [name] + [element['completed']] + [element['title']])
-
+    all_dicts = []
+    for i in all_tasks:
+        temp = {}
+        temp['task'] = i['title']
+        temp['completed'] = i['completed']
+        temp['username'] = name
+        all_dicts.append(temp)
+    dictionary = {str(user_id) : all_dicts}
+    with open(f'{user_id}.json', 'w') as file:
+        dw = json.dump(dictionary, file)
 
 
 if __name__ == '__main__':
