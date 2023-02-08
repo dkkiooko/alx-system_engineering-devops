f0430b624842d683a44fbaa71bd1969225936af7
dkkiooko <kithekadankioko@gmail.com>
2023-02-08 12:19:16 +0300

    get information about TODO list progress of employee

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""gathers data from an API"""
+import requests
+from sys import argv
+
+
+user_id = argv[1]
+url = 'https://jsonplaceholder.typicode.com/'
+
+
+def main():
+    r = requests.get('{}users/{}'.format(url, user_id))
+    name = r.json()['name']
+    r2 = requests.get('{}todos?userId={}'.format(url, user_id)).json()
+    all_tasks = [i for i in r2[:]]
+    completed_tasks = [i['title'] for i in r2[:] if i['completed'] is True]
+    no_all = len(all_tasks)
+    no_com = len(completed_tasks)
+    print("Employee {} is node with tasks({}/{})".format(name, no_com, no_all))
+    for element in completed_tasks:
+        print('\t {}'.format(element))
+
+
+if __name__ == '__main__':
+    main()
---
+++ 0x15-api/README.md
@@ -0,0 +1,12 @@
+ 0. Gather data from an API 
+ Python script that uses a REST API https://jsonplaceholder.typicode.com/
+ For a given employee ID, returns information about their TODO list progress
+ 
+ 
+ 1. Export to CSV
+ Using task #0, extends Python script to export data in the CSV format
+ 
+ 
+ 2. Export to JSON
+ Using task #0, extends Python script to export data in the JSON format
+ 
